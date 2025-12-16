ba3117bc9e55d9e0198e022f4492362293ef1f20
Romik <sojitramanisha@gmail.com>
2025-12-16 15:53:03 +0530

    add codes

--- src/Utils/response.utils.js
+++ src/Utils/response.utils.js
@@ -3,6 +3,17 @@ const I18nUtils = require("./i18n.utils");
 class ResponseUtils {
 
     static i18n = I18nUtils;
+
+    static ERROR_CODES = {
+        SUCCESS: { code: 'SUCCESS', status: 200 },
+        CREATED: { code: 'CREATED', status: 201 },
+        BAD_REQUEST: { code: 'BAD_REQUEST', status: 400 },
+        UNAUTHORIZED: { code: 'UNAUTHORIZED', status: 401 },
+        FORBIDDEN: { code: 'FORBIDDEN', status: 403 },
+        NOT_FOUND: { code: 'NOT_FOUND', status: 404 },
+        CONFLICT: { code: 'CONFLICT', status: 409 },
+        INTERNAL_SERVER_ERROR: { code: 'INTERNAL_SERVER_ERROR', status: 500 },
+    };
     
     static success({req, res, key, code = null, data = {}}) {
         return res.json({
